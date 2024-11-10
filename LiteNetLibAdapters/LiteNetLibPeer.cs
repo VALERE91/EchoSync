1d3486f5a2cc0e058a8f376c99b146a80801bfb9
Valère Plantevin <valereplantevin@proton.me>
2024-11-10 17:42:44 -0500

    Remove global constructors

--- LiteNetLibAdapters/LiteNetLibPeer.cs
+++ LiteNetLibAdapters/LiteNetLibPeer.cs
@@ -5,16 +5,24 @@ using LiteNetLibAdapters.Senders;
 
 namespace LiteNetLibAdapters
 {
-    public class LiteNetLibPeer(NetPeer peer) : IPeer
+    public class LiteNetLibPeer : IPeer
     {
         private readonly PeerReceiver _peerReceiver = new PeerReceiver();
+        private readonly NetPeer _peer;
+
+        public LiteNetLibPeer(NetPeer peer)
+        {
+            _peer = peer;
+            Sender = new PeerSender(peer);
+        }
+
         public IPacketReceiver Receiver => _peerReceiver;
-        public IPacketSender Sender { get; set; } = new PeerSender(peer);
+        public IPacketSender Sender { get; set; }
         public Guid Identifier { get; set; } = Guid.NewGuid();
 
         public void Dispose()
         {
-            peer.Disconnect();
+            _peer.Disconnect();
         }
 
         public void QueueData(NetPacketReader reader, byte channel, DeliveryMethod deliveryMethod)
